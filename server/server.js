de98bf4cd20933acba684144745e1c649295a02d
jnsw <jns.w@icloud.com>
2020-12-22 15:36:56 +0800

    server updates

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express')
-const server = express()
+const app = express()
+const server = require('http').createServer(app)
 require('dotenv').config()
 require('./lib/connection')
 const cors = require('cors')
 const passport = require('./lib/auth')
 
-server.use(express.json())
-server.use(cors())
+const io = require('socket.io')
+// app.use(server.json())
+app.use(cors())
 
+app.use('/api/chat', require('./routes/chats.routes'))
 
-server.use('/chat', require('./routes/chats.routes'))
+// io.on('connection', socket => { /* ... */ })
 
 server.listen(process.env.PORT, () => {
   console.log(`Running server on port ${process.env.PORT}`)
